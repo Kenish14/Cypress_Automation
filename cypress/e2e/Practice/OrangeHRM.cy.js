8713c65f13d08dd3c360fabd1ebc3c7072374ac6
Kenish14 <kenishhalani@gmail.com>
2023-10-16 21:42:15 -0400

    Changes to the file Added test case to visit the URL

--- cypress/e2e/Practice/OrangeHRM.cy.js
+++ cypress/e2e/Practice/OrangeHRM.cy.js
@@ -1,5 +1,6 @@
 describe("Validating and Practicing the functionalities on Orange HRM",()=>{
     it("Test Cases",()=>{
+        //Test all the functionalities on the website and validate them as well.
         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
     })
 })
