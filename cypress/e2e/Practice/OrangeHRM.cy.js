17bc40c1902bd61d601ecedd36d9e1ae6e7d013c
Kenish14 <kenishhalani@gmail.com>
2023-10-16 21:40:47 -0400

    Making Practice folder for all the practice files.

---
+++ cypress/e2e/Practice/OrangeHRM.cy.js
@@ -0,0 +1,5 @@
+describe("Validating and Practicing the functionalities on Orange HRM",()=>{
+    it("Test Cases",()=>{
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    })
+})
